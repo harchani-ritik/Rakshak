58b9939b6d639b6b0b293312708c01638be7298d
abhiandthetruth <abhiandthetruth@gmail.com>
2020-02-08 03:31:22 +0530

    initial backend setup

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
-ncbjh
+var express = require("express");
+var app = express();
+global.__root = __dirname + "/";
+
+app.get("/api", function(req, res) {
+  res.status(200).send("API works.");
+});
+
+
+module.exports = app;
