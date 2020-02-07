86e9d578a19c97e24ebc465da54db839cc7387b7
Ritik Harchani <harchani.ritik@gmail.com>
2020-02-08 04:28:53 +0530

    Add calling button

--- app/src/main/java/com/bitsplease/rakshak/MainActivity.java
+++ app/src/main/java/com/bitsplease/rakshak/MainActivity.java
@@ -1,14 +1,58 @@
 package com.bitsplease.rakshak;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int REQUEST_PHONE_CALL=1;
+    Button helpButton;
+    
+    @SuppressLint("MissingPermission")
+    void makeCall()
+    {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "100"));
+        startActivity(intent);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_PHONE_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makeCall();
+            } else {
+                Toast.makeText(MainActivity.this, "Calling Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        helpButton = findViewById(R.id.help_button);
+        ((View) helpButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this,"Grant Permission to make calls",Toast.LENGTH_SHORT).show();
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+                }
+                else {
+                    makeCall();
+                }
+            }
+        });
+
     }
 }
