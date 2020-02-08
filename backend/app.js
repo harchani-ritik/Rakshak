a6598852072d899cdae6b50648de6e758034d15c
abhiandthetruth <abhiandthetruth@gmail.com>
2020-02-08 11:25:19 +0530

    tokens recieved message send

--- backend/app.js
+++ backend/app.js
@@ -15,44 +15,64 @@ admin.initializeApp({
   databaseURL: "https://rakshak-53755.firebaseio.com"
 });
 
+var db = admin.database();
+var ref = db.ref("users");
 
-app.get("/", function(req, res) {
-var registrationToken = 'c-e1XFgMDro:APA91bFuEg1tnwP8p66INxrhdDGOEK2KYvL5UK3IYpGMkM7Hl-LvWwrpJhBkZ2Fn8kzosHxN42bJ_DREYK4Yhkas4hEPO2r7JnEJNcj1rMsW51OLWqvKn0aMM6I2IfV6SSPqiShddzmw';
-var message = {
-  data: {
-    date: 'Aastha',
-    score: '850',
-    time: '2:45'
-  },
-  token: registrationToken
+const messsage = (registrationToken, location, date, type, time) => {
+  var message = {
+    data: {
+      loc : location,
+      date: date,
+      type: type,
+      time: time
+    },
+    token: registrationToken
+  };
+  // Send a message to the device corresponding to the provided
+  // registration token.
+  admin.messaging().send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+    });
+    res.status(200).send("Yes");
 };
 
-// Send a message to the device corresponding to the provided
-// registration token.
-admin.messaging().send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
-  });
+const isViable = (uid) => {
+  return true;
+};
+
+const isValid = (body) => {
+  return true;
+};
+
+
+app.get("/", function(req, res) {
   res.status(200).send("Yes");
 });
 
 app.post("/requests", function(req, res){
-  // if(!isValid(req.body)) res.status(404).send("invalid request");
-  // switch(req.body.type){
-  //   case "F":
-  // }
-  admin.auth().listUsers(10)
-    .then(function(listUsersResult) {
-      listUsersResult.users.forEach(function(userRecord) {
-        console.log('user', userRecord.toJSON());
-      });
-    }).catch(function(error) {
-    console.log('Error fetching user data:', error);
+  //checking for a valid request
+  if(!isValid(req.body)) res.status(404).send("invalid request");
+  
+  //getting the uid
+  let uid = req.body.uid;
+  
+  //getting all the registrationTokens and sending the notification
+  ref.on("value", function(snapshot) {
+    let tokens = snapshot.val();
+    Object.keys(tokens).forEach(function(key) {
+      //Decide whether the current key is viable for sending the message
+      if (key!==uid && isViable(key)) message(tokens[key], req.body.loc, req.body.date, req.body.type, req.body.time);
+    });
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
   });
+
+  //the response
   res.status(200).send(req.body);
 });
 module.exports = app;
